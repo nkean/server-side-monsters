fa00b7a5ebe0428caca82100d30f8020c40f04a0
nkean <nathankean2@gmail.com>
2018-04-18 17:17:49 -0500

    server responds on request to '/monsters'

--- server/server.js
+++ server/server.js
@@ -2,4 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-const monster = require('./modules/monsters')
+const monster = require('./modules/monsters');
+
+app.use(express.static('server/public'));
+
+app.get('/monsters', (req, res) => {
+    res.send(monster);
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+});
